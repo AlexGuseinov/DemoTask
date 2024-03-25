68d68c78b6b8f0598268e6b724ded6d0a8a79bcf
AlexGuseinov <ali.guseynof2503@gmail.com>
2024-03-25 03:08:55 -0700

    some minor changes

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,10 @@
+using Application;
+using Application.CrossCuttingConcerns.Exceptions.Extensions;
+using Infrastructure;
+using Microsoft.OpenApi.Models;
+using Persistence;
+using MailService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +12,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo APP", Version = "v1" });
+});
+
+builder.Services.AddApplicationDepdendencies(builder.Configuration);
+builder.Services.AddPersistenceDependencies(builder.Configuration);
+builder.Services.AddInfrastructureDependencies();
+builder.Services.AddMailService(builder.Configuration);
 
 var app = builder.Build();
 
@@ -16,7 +31,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
 app.UseHttpsRedirection();
+
+app.UseExceptionHandling();
 
 app.UseAuthorization();
 
